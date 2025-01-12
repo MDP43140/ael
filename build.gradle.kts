56b235074bee760015a23e8b790e679be47a9310
MDP43140 <68391650+MDP43140@users.noreply.github.com>
2025-01-12 07:57:43 +0700

    Update dependency
    + Gradle wrapper: 8.11.1 > 8.12
    - com.android.*: 8.7.2 > 8.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ allprojects {
 }
 plugins {
 //auto suffixed with .gradle.plugin
-	id("com.android.application") version "8.7.2" apply false
-	id("com.android.library") version "8.7.2" apply false
+	id("com.android.application") version "8.7.3" apply false
+	id("com.android.library") version "8.7.3" apply false
 	id("maven-publish")
 	kotlin("android") version "2.1.0" apply false // kotlin("android") == "org.jetbrains.kotlin.android"
 }
